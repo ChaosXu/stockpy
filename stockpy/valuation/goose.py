b2f243adbf3be9813cb2e9b6809fc47244588818
ChaosXu <jun.chaos@gmail.com>
2020-01-19 19:50:15 +0800

    add:skeleton of goose

--- stockpy/valuation/goose.py
+++ stockpy/valuation/goose.py
@@ -1,20 +1,39 @@
 from stockpy.meta import MetricsMeta
-from stockpy.expr import Value, Sum, Div, Get, Range, Before
+from stockpy import expr
+
+
+def market_cap():
+    ''' 估算总市值
+    '''
+    return MetricsMeta('e_market_cap.goose',
+                       expr.Multi(expr.Get('total_hldr_eqy_exc_min_int'),
+                                  expr.Sum(expr.Value(1),
+                                           expr.Get('f_roe_ttm'))))
+
+
+def moat_factor():
+    ''' 护城河等级系数
+    '''
+    return 1
+
+
+def safety_factor():
+    '''安全边际系数
+        50%到90%
+    '''
+    return 1
+
+
+def price_share():
+    '''估算单价'''
+    return MetricsMeta('e_price_share.goose',
+                       expr.Div(expr.Get('e_market_cap.goose'),
+                                expr.Get('total_share')))
 
 
 def metrics():
     metas = [
-        # 滚动净利润：连续12个月
-        MetricsMeta('f_income.ttm', Sum(
-            Range(Get('n_income'), quarter=4))),
-        # 滚动ROE get(n_income_ttm) / ((get(total_hldr_eqy_inc_min_int) + get(total_hldr_eqy_inc_min_int,-4))/2)
-        MetricsMeta('roe.ttm',
-                    Div(Get('f_income.ttm'),
-                        Div(
-                            Sum(Get('total_hldr_eqy_inc_min_int'),
-                                Before(Get('total_hldr_eqy_inc_min_int'),
-                                       past_quarter=4),
-                                ),
-                            Value(2))))
+        market_cap(),
+        price()
     ]
     return metas
